6cbd71458392775d445c5f7b554ba0742fd01f65
gabrielhipolitoo <c.gabrielhipolitoh@gmail.com>
2024-07-02 20:46:13 -0300

    fim aula 4

---
+++ aula4/numberBigint.js
@@ -0,0 +1,8 @@
+"use strict";
+let num1 = 0x3435;
+let num2 = 0x232;
+let num3 = 123.0;
+let num4 = 0o124333230;
+let big1 = 900000n;
+let big2 = 3n;
+console.log(num4);
---
+++ aula4/numberBigint.ts
@@ -0,0 +1,9 @@
+let num1: number = 0x3435;
+let num2: number = 0x232;
+let num3: number = 123.0;
+let num4: number = 0o124333230;
+
+let big1: bigint = 900000n;
+let big2: bigint = 0b11n;
+
+console.log(num4);
